13d1570ae93223ecd6472bbce7dba1820f26208f
Abdul Mufid <abdmufid75@gmail.com>
2024-01-18 11:05:29 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var i = document.getElementById("toggleLinks");
+  if (i.style.display === "block") {
+    i.style.display = "none";
+  } else {
+    i.style.display = "block";
+  }
+}
